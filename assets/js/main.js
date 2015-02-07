15489ee9cd335d48be736127923cb7706ea03a99
Patrick Kinsella <patrick.j.kinsella@gmail.com>
2015-02-07 15:22:50 -0500

    Separated js from index.html.

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+var doughnutData = [
+{
+  value: 80,
+  color:"#1abc9c"
+},
+{
+  value : 20,
+  color : "#ecf0f1"
+}
+];
+var myDoughnut = new Chart(document.getElementById("solidworks").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+{
+  value: 70,
+  color:"#1abc9c"
+},
+{
+  value : 30,
+  color : "#ecf0f1"
+}
+];
+var myDoughnut = new Chart(document.getElementById("feaanalysis").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+{
+  value: 80,
+  color:"#1abc9c"
+},
+{
+  value : 20,
+  color : "#ecf0f1"
+}
+];
+var myDoughnut = new Chart(document.getElementById("gdt").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+{
+  value: 80,
+  color:"#1abc9c"
+},
+{
+  value : 20,
+  color : "#ecf0f1"
+}
+];
+var myDoughnut = new Chart(document.getElementById("machinedesign").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+{
+  value: 90,
+  color:"#1abc9c"
+},
+{
+  value : 10,
+  color : "#ecf0f1"
+}
+];
+var myDoughnut = new Chart(document.getElementById("htmlcss").getContext("2d")).Doughnut(doughnutData);
+
+var doughnutData = [
+{
+  value: 50,
+  color:"#1abc9c"
+},
+{
+  value : 50,
+  color : "#ecf0f1"
+}
+];
+var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
